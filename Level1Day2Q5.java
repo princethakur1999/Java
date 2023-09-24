ab5d27f62f771d437481d2aeb1fc515714dfb73c
Princekumar <pk181299@gmail.com>
2023-09-24 11:30:11 +0530

    Java Program to Find GCD or HCF of 2 numbers.

---
+++ Level1Day2Q5.java
@@ -0,0 +1,30 @@
+//Java Program to Find GCD or HCF of 2 numbers.
+
+import java.util.*;
+
+public class Level1Day2Q5 {
+
+    public static int calculateGCD(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return calculateGCD(b, a % b);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        // Calculate the GCD
+        int gcd = calculateGCD(num1, num2);
+
+        // Display the GCD
+        System.out.println("GCD of " + num1 + " and " + num2 + " is: " + gcd);
+
+    }
+}
