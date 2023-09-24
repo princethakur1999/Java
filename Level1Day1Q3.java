6e66896b372f4f50c92c16632d13e1556abc6a90
Princekumar <pk181299@gmail.com>
2023-09-24 11:13:04 +0530

    Java Program to print Multiplication of two floating point Number.

---
+++ Level1Day1Q3.java
@@ -0,0 +1,15 @@
+//Java Program to print Multiplication of two floating point Number.
+
+class Level1Day1Q3 {
+
+    public static void main(String[] args) {
+
+        // f is to ensures that numbers are float DATA TYPE
+        float f1 = 1.5f;
+        float f2 = 2.0f;
+
+        float p = f1 * f2;
+
+        System.out.println("Product is: " + p);
+    }
+}
