9d1abb5239b23e5bf0cf7b17c715c5abe6d79b49
Princekumar <pk181299@gmail.com>
2023-09-24 11:24:28 +0530

    Java Program to Check if Given Integer is Odd or Even.

---
+++ Level1Day2Q2.java
@@ -0,0 +1,20 @@
+//Java Program to Check if Given Integer is Odd or Even.
+
+public class Level1Day2Q2 {
+
+    public static void main(String[] args) {
+
+        // Declaring and initializing integer variable
+        int num = 10;
+
+        // Checking if number is even or odd number
+        if (num % 2 == 0) {
+
+            System.out.println("The Number is Even");
+
+        } else {
+
+            System.out.println("The Number is Odd");
+        }
+    }
+}
