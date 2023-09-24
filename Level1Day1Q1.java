0cff03cdc8eaf6023fb9c8f0dd1243688b35904f
Princekumar <pk181299@gmail.com>
2023-09-24 11:08:29 +0530

    Java program to take an integer as input and print it.

---
+++ Level1Day1Q1.java
@@ -0,0 +1,16 @@
+//Java program to take an integer as input and print it.
+
+import java.util.Scanner;
+
+public class Level1Day1Q1 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        System.out.println("The number is: " + num);
+    }
+}
