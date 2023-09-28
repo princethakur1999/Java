f86d7ab0b5df396e0cfb95a2cdc2917ddfd70f91
Princekumar <pk181299@gmail.com>
2023-09-28 23:16:19 +0530

    Java program to compute imple interest.

---
+++ Level1Day4Q3.java
@@ -0,0 +1,14 @@
+//Java program to compute imple interest.
+
+class Level1Day4Q3 {
+    public static void main(String args[]) {
+
+        float P = 100, R = 1, T = 1;
+
+        float SI = (P * T * R) / 100;
+
+        System.out.println("Simple interest = " + SI);
+    }
+}
+
+// This code is contributed by Anant Agarwal.
