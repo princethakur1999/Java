c86a327917956594059cc5248a2c2cb337c1bb10
Princekumar <pk181299@gmail.com>
2023-10-09 10:07:02 +0530

    Java program to add two binary strings.

--- Level1Day1Q5.java
+++ Level1Day1Q5.java
@@ -4,16 +4,19 @@ public class Level1Day1Q5 {
 
     public static void main(String[] args) {
 
-        int n1 = Integer.parseInt("1111", 2);
-        int n2 = Integer.parseInt("1001", 2);
+        int num1 = Integer.parseInt("1111", 2);
+        int num2 = Integer.parseInt("1100", 2);
 
-        int sum = n1 + n2;
+        System.out.println(num1);
+        System.out.println(num2);
+
+        int sum = num1 + num2;
+
+        System.out.println(sum);
 
         String result = Integer.toBinaryString(sum);
 
-        System.err.println(n1);
-        System.out.println(n2);
-        System.out.println(sum);
         System.out.println(result);
+
     }
 }
